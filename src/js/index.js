289316d730025e2a7b91a255f329b73d1be654cb
Vladyslab Lavrov <lavrowlad@gmail.com>
2021-07-01 20:23:29 +0300

    Write styles for Recent-posts sections. Edit html for Recent-posts section.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 @@include('burger.js');
+
+
+const app = document.querySelector('#app');
+
+const threeDepth = (el, currentLevel = 0) => {
+  if (!el) throw new Error("Can't have element");
+  if (el.children.length === 0) return currentLevel;
+  
+  let maxDepth = currentLevel;
+  currentLevel++;
+
+  Array.from(el.children).forEach(element => {
+    maxDepth = Math.max(threeDepth(element, currentLevel), maxDepth);
+  })
+
+  return maxDepth;
+}
+
+console.log(threeDepth(app));
