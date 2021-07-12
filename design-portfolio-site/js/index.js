cd2b3ca3b0435355732cea762ce8a85ee16cf30c
Vladyslab Lavrov <lavrowlad@gmail.com>
2021-07-12 15:05:37 +0300

    generate site files for prodaction

--- design-portfolio-site/js/index.js
+++ design-portfolio-site/js/index.js
@@ -8,3 +8,20 @@ menu_icon.addEventListener('click', (e) => {
   menu_icon.classList.toggle('active')
   menu_body.classList.toggle('active')
 });
+const map = document.querySelector('.map');
+const addresses = Array.from(map.querySelectorAll(".map__address"))
+const images = Array.from(map.querySelectorAll(".map__preview img"))
+
+addresses.forEach((address, index) => {
+  address.addEventListener('click', (e) => {
+    e.preventDefault;
+
+    addresses.forEach(address => address.classList.remove('map__address-active'))
+    images.forEach(img => img.classList.remove('view'))
+    
+    let currentImg = e.target.id.split('-')[1] - 1;
+
+    e.target.classList.add('map__address-active');
+    images[currentImg].classList.add('view');
+  })
+});
